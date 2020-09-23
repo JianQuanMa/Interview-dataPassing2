5dbbceb97c8b5fc2837df62e8f877bd4aefb50e0
Jian Ma <jianquanma@gmail.com>
2020-09-23 19:27:57 -0400

    pushing all default classes

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interview-dataPassing2
+//
+//  Created by Jian Ma on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //added something   
+    }
+
+
+}
+
